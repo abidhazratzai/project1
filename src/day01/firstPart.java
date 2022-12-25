ff7e945ac3b9c76d8d8c360111020d43a4e83b39
abidhazratzai <Abidhazratzai007@gmail.com>
2022-12-24 22:59:26 -0500

    project1Added

---
+++ src/day01/firstPart.java
@@ -0,0 +1,8 @@
+package day01;
+
+public class firstPart {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("my name is khan");
+    }
+}
